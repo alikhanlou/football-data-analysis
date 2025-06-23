827cceb19b4ac5bf2e70ede4f6ea016f7f466e6c
Ali MohammadKhanlou <ali.khanlou.ml@gmail.com>
2025-06-23 15:33:35 +0330

    new2

--- notebooks/player_analysis.py
+++ notebooks/player_analysis.py
@@ -6,6 +6,8 @@ import seaborn as sns
 players_data = Path(__file__).resolve().parent.parent / "data" / "raw" / "top5_leagues_players_2023.csv"
 df = pd.read_csv(players_data)
 
+
+
 print(df.head())
 print("--------------------\n--------------------")
 print(df.info())
@@ -15,4 +17,55 @@ print("--------------------\n--------------------")
 print(df.corr(numeric_only=True))
 print("--------------------\n--------------------")
 print(df.isnull().sum())
+print("--------------------\n--------------------")
 
+
+
+df["custom_rating"] = (
+    df["goals"] * 4 +
+    df["assists"] * 3 +
+    df["shots"] * 0.5 -
+    df["yellow_cards"] -
+    df["red_cards"] * 3
+)
+top_player = df.sort_values(by="custom_rating",ascending=False).head(1)
+print(top_player[["name","club","age","goals","assists","custom_rating"]])
+
+
+print("--------------------\n--------------------")
+
+
+goals_by_position = df.groupby("position")["goals"].sum().sort_values(ascending=False)
+sns.barplot(x=goals_by_position.values, y=goals_by_position.index)
+plt.title("Total Goals by Position")
+plt.xlabel("Goals")
+plt.ylabel("Position")
+plt.tight_layout()
+plt.show()
+
+
+
+
+sns.scatterplot(x="minutes", y="custom_rating", data=df)
+plt.title("Rating vs Minutes Played")
+plt.xlabel("Minutes Played")
+plt.ylabel("Custom Rating")
+plt.tight_layout()
+plt.show()
+
+
+
+position_stats = df.groupby("position").agg({
+    "goals": "mean",
+    "assists": "mean",
+    "custom_rating": "mean"
+}).sort_values(by="custom_rating", ascending=False)
+print(position_stats)
+
+
+
+
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm")
+plt.title("Correlation Matrix")
+plt.tight_layout()
+plt.show()
